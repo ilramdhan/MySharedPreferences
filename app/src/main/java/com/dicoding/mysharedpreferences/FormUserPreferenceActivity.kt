8d6385909bf8cfd936ec3d715ca75f4df38ba8f6
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-25 22:00:07 +0700

    Add user preference

---
+++ app/src/main/java/com/dicoding/mysharedpreferences/FormUserPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.mysharedpreferences
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+    }
+}
